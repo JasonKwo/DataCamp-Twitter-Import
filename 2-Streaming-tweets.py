5329517cca8cd05b44aff324bbe2276d6c718f9c
Jason Kwo <51009700+JasonKwo@users.noreply.github.com>
2019-09-09 11:37:57 +1000

    Create 2-Streaming-tweets.py
    Your task is to create the 'Stream' object and to filter tweets according to particular keywords.

---
+++ 2-Streaming-tweets.py
@@ -0,0 +1,10 @@
+# MyStreamListener os the tweet stream listener class defined by Hugo.
+
+# Initialize Stream listener
+l = MyStreamListener()
+
+# Create your Stream object with authentication
+stream = tweepy.Stream(auth, l)
+
+# Filter Twitter Streams to capture data by the keywords:
+stream.filter(track=['clinton','trump','sanders','cruz'])
