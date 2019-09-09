e4d75eaf96468eb65b4a2a400f62e80aac51fa13
Jason Kwo <51009700+JasonKwo@users.noreply.github.com>
2019-09-09 11:43:15 +1000

    Create 3-Load-and-explore-your-Twitter-data.py

---
+++ 3-Load-and-explore-your-Twitter-data.py
@@ -0,0 +1,27 @@
+# The prior sections have connected to twitter using an OAuth handler, created a file 'tweets.txt' and filtered for 
+# keywords = ['clinton','trump','sanders','cruz']
+
+# In this exercise we will read the Twitter data into a list 'tweets_data'
+
+# Import package
+import json
+
+# String of path to file: tweets_data_path
+tweets_data_path = 'tweets.txt'
+
+# Initialize empty list to store tweets: tweets_data
+tweets_data=[]
+
+# Open connection to file
+tweets_file = open(tweets_data_path, "r")
+
+# Read in tweets and store in list: tweets_data
+for line in tweets_file:
+    tweet = json.loads(line)
+    tweets_data.append(tweet)
+
+# Close connection to file
+tweets_file.close()
+
+# Print the keys of the first tweet dict
+print(tweets_data[0].keys())
